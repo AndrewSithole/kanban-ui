f586a3c137e0f54551ae76cf8d6566d40645d2b9
Andrew Sithole <andrewsithole7@gmail.com>
2024-01-15 22:10:07 +0200

    Added mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import './App.css'
+import TicketCard from "./components/ticketCard.tsx";
+import {tickets} from "./data/tickets.ts";
 
 function App() {
-    const title = "Do market research";
-    const id = "BUS-1";
-    const description = "Desctiption";
-    const assignedTo = "Andrew";
+
     return (
-        <div className="border rounded-lg p-2 m-2 bg-gray-50">
-            <div className="title font-semibold pb-1">{title}</div>
-            <div className="descripition pb-1">{description}</div>
-            <div className="flex justify-between pb-1 text-gray-700">
-                <div>{id}</div>
-                <div className="flex gap-2"><span className="bi bi-person"></span>{assignedTo}</div>
-            </div>
-        </div>
+        <>
+            { tickets.map(ticket=> <TicketCard key={ticket.id} ticket={ticket}></TicketCard>)}
+        </>
     )
 }
 
