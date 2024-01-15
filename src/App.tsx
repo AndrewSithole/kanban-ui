a653dc5ba4c532e898a2c69b184f7e394d8bc86f
Andrew Sithole <andrewsithole7@gmail.com>
2024-01-15 21:36:59 +0200

    Designed ticket

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
 
 function App() {
-
+    const title = "Do market research";
+    const id = "BUS-1";
+    const description = "Desctiption";
+    const assignedTo = "Andrew";
     return (
-        <div className="text-4xl text-red-500">
-            Hello
+        <div className="border rounded-lg p-2 m-2 bg-gray-50">
+            <div className="title font-semibold pb-1">{title}</div>
+            <div className="descripition pb-1">{description}</div>
+            <div className="flex justify-between pb-1 text-gray-700">
+                <div>{id}</div>
+                <div className="flex gap-2"><span className="bi bi-person"></span>{assignedTo}</div>
+            </div>
         </div>
     )
 }
